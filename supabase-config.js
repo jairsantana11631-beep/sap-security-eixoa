0e0b24891dd536166fd54be1160747ff57f56824
jairsantana11631-beep <jairsantana11631@gmail.com>
2026-09-15 17:42:02 -0300

    Update comment in supabase-config.js
    Corrected a comment regarding the use of the Publishable Key.

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,6 @@
+</> Java Script
 // SAP Security Eixo - Configuracao Supabase
-// Use somente a Publishable Key. Nunca coloque Secret Key aqui.
-
-</> Java 
+// Use somente a Publishable Key. Nunca coloque Secret Key aq
 window.SUPABASE_CONFIG = {
   SUPABASE_URL: "https://jytxjbrdemirstrgznft.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_x7Z4V0TcnKWa01h0q6QrkA_cAmfR0VZ"
