4b37f3d1d5e00b1a72bcba5de51a79a5b8862895
jairsantana11631-beep <jairsantana11631@gmail.com>
2026-09-15 17:09:29 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,15 @@
+// SAP Security Eixo - Configuracao Supabase
+// Use somente a Publishable Key. Nunca coloque Secret Key aqui.
+
+</> Java Script
+
+
+  window.SUPABASE_CONFIG = {
+  SUPABASE_URL: "https://jytxjbrdemirstrgznft.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_SUA_CHAVE_COMPLETA"
+};   
+
+window.SUPABASE_CONFIG = {
+  SUPABASE_URL: "https://jytxjbrdemirstrgznft.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "COLE_AQUI_SUA_PUBLISHABLE_KEY"
+};
